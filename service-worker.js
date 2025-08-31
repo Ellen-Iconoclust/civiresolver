3c6e5b15cf998fe7fe92b60f03a40056d98c07b5
Ellen-Iconoclust <elleniconoclust@gmail.com>
2025-08-31 11:59:31 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'citywatch-v1.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
